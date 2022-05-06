9a796b3a60abf7f83509a373c87824b76f38d074
7n9 <k0h3z@pm.me>
2022-05-06 15:14:03 +0200

    nn

---
+++ lab-04-polbud/output/CMakeFiles/lab-04-polbud.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab-04-polbud.dir/lab-04-polbud.c.obj"
+  "CMakeFiles/lab-04-polbud.dir/lab-04-polbud.c.obj.d"
+  "lab-04-polbud.exe"
+  "lab-04-polbud.exe.manifest"
+  "lab-04-polbud.pdb"
+  "liblab-04-polbud.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab-04-polbud.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab-04-polbud/output/CMakeFiles/lab-04-polbud.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for lab-04-polbud.
+# This may be replaced when dependencies are built.
